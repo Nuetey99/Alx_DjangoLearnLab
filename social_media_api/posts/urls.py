dc2118e135067d83433e492427f08085544be23e
nuetey99 <nueteyn03@gmail.com>
2024-09-17 11:27:05 +0000

    task 2

---
+++ social_media_api/posts/urls.py
@@ -0,0 +1,35 @@
+"""
+URL configuration for social_media_api project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+# accounts/urls.py
+# posts/urls.py
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from ..accounts.views import PostViewSet, CommentViewSet
+from .views import FollowUserView, UnfollowUserView , UserFeedView
+
+router = DefaultRouter()
+router.register(r'posts', PostViewSet)
+router.register(r'comments', CommentViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+    path('api/accounts/', include('accounts.urls')),
+    path('api/', include('posts.urls')),
+    path('follow/<int:user_id>/', FollowUserView.as_view(), name='follow-user'),
+    path('unfollow/<int:user_id>/', UnfollowUserView.as_view(), name='unfollow-user'),
+     path('feed/', UserFeedView.as_view(), name='user-feed'),
+]
