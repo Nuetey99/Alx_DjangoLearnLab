ad0fb6168d3c1bd2c87786571d1e66ce35d4f4e4
nuetey99 <nueteyn03@gmail.com>
2024-09-17 11:16:28 +0000

    task 1

--- social_media_api/social_media_api/urls.py
+++ social_media_api/social_media_api/urls.py
@@ -18,7 +18,7 @@ Including another URLconf
 # posts/urls.py
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import PostViewSet, CommentViewSet
+from ..accounts.views import PostViewSet, CommentViewSet
 
 router = DefaultRouter()
 router.register(r'posts', PostViewSet)
