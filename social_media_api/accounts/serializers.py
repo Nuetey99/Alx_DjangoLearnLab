bc44b687ceb5f92afc985152ee91d65a132d2196
nuetey99 <nueteyn03@gmail.com>
2024-09-17 10:58:16 +0000

    task 0

---
+++ social_media_api/accounts/serializers.py
@@ -0,0 +1,21 @@
+# accounts/serializers.py
+from rest_framework import serializers
+from .models import CustomUser
+from django.contrib.auth import get_user_model
+
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = get_user_model()
+        fields = ('username', 'password', 'email', 'bio', 'profile_picture')
+
+    def create(self, validated_data):
+        user = CustomUser.objects.create_user(
+            username=validated_data['username'],
+            password=validated_data['password'],
+            email=validated_data.get('email', ''),
+            bio=validated_data.get('bio', ''),
+            profile_picture=validated_data.get('profile_picture', None)
+        )
+        return user
---
+++ social_media_api/accounts/views.py
@@ -0,0 +1,25 @@
+# accounts/views.py
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.authtoken.models import Token
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.permissions import AllowAny
+from .serializers import UserRegistrationSerializer
+
+class RegisterUser(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        serializer = UserRegistrationSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            token, created = Token.objects.get_or_create(user=user)
+            return Response({'token': token.key}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class CustomAuthToken(ObtainAuthToken):
+    def post(self, request, *args, **kwargs):
+        response = super().post(request, *args, **kwargs)
+        token = response.data['token']
+        return Response({'token': token})
